e39e7b73123cb41c9374182d757f38dd606b453b
Samuel Bader <samuel.bader@carbon-it.com>
2022-11-09 11:03:53 +0100

    3-refacto: Add the sum to the stack given a sequence with two number and a plus

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
+function sum(stack) {
+    const first = stack.pop();
+    const second = stack.pop();
+    stack.push(first + second);
+}
+
 function rpn(sequence) {
     const parsedSequence = sequence.split(" ");
     const stack = [];
     parsedSequence.forEach((element) => {
         const number = parseInt(element);
-        if (!isNaN(number)) {
+        if (isNaN(number)) {
+            if (element === '+') {
+                sum(stack);
+            }
+        } else {
             stack.push(number);
         }
     })

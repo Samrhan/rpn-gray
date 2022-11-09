610a2e9573e23d613b9a5559b23456ca1f327be6
Samuel Bader <samuel.bader@carbon-it.com>
2022-11-09 11:10:17 +0100

    4-test: Support multiple operation

--- test/main.test.js
+++ test/main.test.js
@@ -1,7 +1,7 @@
 const rpn = require('../main')
 
 describe('RPN', () => {
-    it("should an empty array on empty squence", () => {
+    it("should an empty array on empty sequence", () => {
         expect(rpn('')).toEqual([]);
     })
     it('should add one number to the stack given a sequence with one number', () => {
@@ -15,6 +15,9 @@ describe('RPN', () => {
     });
     it('should add the difference to the stack given a sequence with two numbers and a -', () => {
         expect(rpn('2 1 -')).toEqual([1]);
+    });
+    it('should support multiple operations', () => {
+        expect(rpn('4 2 + 3 -')).toEqual([3]);
     });
 
 })

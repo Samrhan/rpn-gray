826288ae5674cf5aa5c379127c1c49f47ef03a17
Samuel Bader <samuel.bader@carbon-it.com>
2022-11-09 10:57:44 +0100

    2-test: Add n number to the stack given a sequence with n number

--- test/main.test.js
+++ test/main.test.js
@@ -7,4 +7,7 @@ describe('RPN', ()=>{
     it('should add one number to the stack given a sequence with one number', ()=>{
         expect(rpn('1')).toEqual([1]);
     })
+    it("should add n numbers to the stack given a sequence with n numbers", ()=>{
+        expect(rpn('1 2 3 4 5')).toEqual([1,2,3,4,5]);
+    });
 })

3b7b9752dccd4d9c15deca0808ba8eef65544503
Samuel Bader <samuel.bader@carbon-it.com>
2022-11-09 10:57:08 +0100

    1-refacto: Add a number to the stack given a sequence with one number

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
-function rpn(sequence){
-    return []
+function rpn(sequence) {
+    const number = Number.parseInt(sequence);
+    if (Number.isNaN(number)) {
+        return []
+    } else {
+        return [number]
+    }
 }
 
 module.exports = rpn

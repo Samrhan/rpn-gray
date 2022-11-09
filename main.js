bc0cd3d71360de9d5e41bad0a5bcbe08aa7696bb
Samuel Bader <samuel.bader@carbon-it.com>
2022-11-09 11:08:51 +0100

    4-refacto: Add the difference to the stack given a sequence with two number and a minus

--- main.js
+++ main.js
@@ -4,14 +4,28 @@ function sum(stack) {
     stack.push(first + second);
 }
 
+function minus(stack) {
+    const first = stack.pop();
+    const second = stack.pop();
+    // The order of the operands is reversed
+    stack.push(second - first);
+}
+
 function rpn(sequence) {
     const parsedSequence = sequence.split(" ");
     const stack = [];
     parsedSequence.forEach((element) => {
         const number = parseInt(element);
         if (isNaN(number)) {
-            if (element === '+') {
-                sum(stack);
+            switch (element) {
+                case '+':
+                    sum(stack);
+                    break;
+                case '-':
+                    minus(stack);
+                    break;
+                default:
+                    break;
             }
         } else {
             stack.push(number);

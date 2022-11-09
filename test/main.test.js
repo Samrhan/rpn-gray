37bb6ee669a68180979a365988c16a78aef0816c
Samuel Bader <samuel.bader@carbon-it.com>
2022-11-09 11:01:33 +0100

    3-test: Add the sum to the stack given a sequence with two number and a plus

--- test/main.test.js
+++ test/main.test.js
@@ -1,13 +1,16 @@
 const rpn = require('../main')
 
-describe('RPN', ()=>{
-    it("should an empty array on empty squence", ()=>{
+describe('RPN', () => {
+    it("should an empty array on empty squence", () => {
         expect(rpn('')).toEqual([]);
     })
-    it('should add one number to the stack given a sequence with one number', ()=>{
+    it('should add one number to the stack given a sequence with one number', () => {
         expect(rpn('1')).toEqual([1]);
     })
-    it("should add n numbers to the stack given a sequence with n numbers", ()=>{
-        expect(rpn('1 2 3 4 5')).toEqual([1,2,3,4,5]);
+    it("should add n numbers to the stack given a sequence with n numbers", () => {
+        expect(rpn('1 2 3 4 5')).toEqual([1, 2, 3, 4, 5]);
     });
+    it('should add the sum to the stack given a sequence with two numbers and a +', () => {
+        expect(rpn('1 2 +')).toEqual([3]);
+    })
 })

9eee36d6617fa909c6695982074bdd1099b6b1c2
Samuel Bader <samuel.bader@carbon-it.com>
2022-11-09 10:56:06 +0100

    1-test: Add a number to the stack given a sequence with one number

--- test/main.test.js
+++ test/main.test.js
@@ -4,4 +4,7 @@ describe('RPN', ()=>{
     it("should an empty array on empty squence", ()=>{
         expect(rpn('')).toEqual([]);
     })
+    it('should add one number to the stack given a sequence with one number', ()=>{
+        expect(rpn('1')).toEqual([1]);
+    })
 })
